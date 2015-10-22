fac9394d3899db567051002ee1c0c82c905418ad
kixounet <cedric.namotte@gmail.com>
2015-10-22 17:37:24 +0200

    adding list todos component

---
+++ src/app/components/list-todos/list-todos.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/angular2';
+
+
+@Component({
+  selector: 'list-todos',
+  templateUrl: 'app/components/list-todos/list-todos.html',
+  styleUrls: ['app/components/list-todos/list-todos.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class ListTodos {
+
+  constructor() {}
+
+}
